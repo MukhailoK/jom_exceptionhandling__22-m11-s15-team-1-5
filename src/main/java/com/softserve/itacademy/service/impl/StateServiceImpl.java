0fc00f963118371436fb94e09448891641fbb6c8
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2023-01-11 11:12:52 +0200

    exception handling for state and task service

--- src/main/java/com/softserve/itacademy/service/impl/StateServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/StateServiceImpl.java
@@ -1,10 +1,13 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.State;
 import com.softserve.itacademy.repository.StateRepository;
 import com.softserve.itacademy.service.StateService;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,31 +22,57 @@ public class StateServiceImpl implements StateService {
 
     @Override
     public State create(State state) {
+        try {
             return stateRepository.save(state);
+        } catch (InvalidDataAccessApiUsageException | IllegalArgumentException e) {
+            throw new NullEntityReferenceException("State cannot be null");
+        }
     }
+
 
     @Override
     public State readById(long id) {
         Optional<State> optional = stateRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        }
+        throw new EntityNotFoundException("Task with id " + id + " not found");
     }
 
     @Override
     public State update(State state) {
-            State oldState = readById(state.getId());
-                return stateRepository.save(state);
+        if (state != null) {
+            State oldState;
+            try {
+                oldState = readById(state.getId());
+            } catch (IllegalArgumentException e) {
+                throw new NullEntityReferenceException("State id cannot be null");
+            }
+            if (oldState != null) {
+                try {
+                    return stateRepository.save(state);
+                } catch (InvalidDataAccessApiUsageException | IllegalArgumentException e) {
+                    throw new NullEntityReferenceException("Sate cannot be null");
+                }
+            }
+        }
+        throw new NullEntityReferenceException("State can`t be 'null'");
     }
 
     @Override
     public void delete(long id) {
         State state = readById(id);
+        if (state != null) {
             stateRepository.delete(state);
+        } else {
+            throw new EntityNotFoundException("Sate with id " + id + " not found");
+        }
     }
 
     @Override
     public State getByName(String name) {
         Optional<State> optional = Optional.ofNullable(stateRepository.getByName(name));
-            return optional.get();
+        return optional.get();
     }
 
     @Override
--- src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
@@ -1,10 +1,15 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
 import com.softserve.itacademy.model.Task;
+import com.softserve.itacademy.model.ToDo;
 import com.softserve.itacademy.repository.TaskRepository;
+import com.softserve.itacademy.repository.ToDoRepository;
 import com.softserve.itacademy.service.TaskService;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +17,7 @@ import java.util.Optional;
 @Service
 public class TaskServiceImpl implements TaskService {
     private TaskRepository taskRepository;
+    private ToDoRepository todoRepository;
 
     public TaskServiceImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
@@ -19,25 +25,50 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Task create(Task user) {
+        try {
             return taskRepository.save(user);
+        } catch (InvalidDataAccessApiUsageException | IllegalArgumentException e) {
+            throw new NullEntityReferenceException("Task cannot be null");
+        }
     }
 
     @Override
     public Task readById(long id) {
         Optional<Task> optional = taskRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        }
+        throw new EntityNotFoundException("Task with id " + id + " not found");
     }
 
     @Override
     public Task update(Task task) {
-            Task oldTask = readById(task.getId());
-                return taskRepository.save(task);
+        if (task != null) {
+            Task oldTask;
+            try {
+                oldTask = readById(task.getId());
+            } catch (IllegalArgumentException e) {
+                throw new NullEntityReferenceException("Task id cannot be null");
+            }
+            if (oldTask != null) {
+                try {
+                    return taskRepository.save(task);
+                } catch (InvalidDataAccessApiUsageException | IllegalArgumentException e) {
+                    throw new NullEntityReferenceException("Task cannot be null");
+                }
+            }
+        }
+        throw new NullEntityReferenceException("Task can`t be 'null'");
     }
 
     @Override
     public void delete(long id) {
         Task task = readById(id);
+        if (task != null) {
             taskRepository.delete(task);
+        } else {
+            throw new EntityNotFoundException("Task with id " + id + " not found");
+        }
     }
 
     @Override
@@ -48,7 +79,11 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<Task> getByTodoId(long todoId) {
-        List<Task> tasks = taskRepository.getByTodoId(todoId);
-        return tasks.isEmpty() ? new ArrayList<>() : tasks;
+        Optional<ToDo> optional = todoRepository.findById(todoId);
+        if (optional.isPresent()) {
+            List<Task> tasks = taskRepository.getByTodoId(todoId);
+            return tasks.isEmpty() ? new ArrayList<>() : tasks;
+        }
+        throw new EntityNotFoundException("Todo with id " + todoId + " not found");
     }
 }
